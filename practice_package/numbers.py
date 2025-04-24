052c892a17a6cc51718e0b977e9ad1a81828b831
p1ep1ev <anna.prav54@gmail.com>
2025-04-24 22:49:15 +0300

    numbers

--- practice_package/numbers.py
+++ practice_package/numbers.py
@@ -1,18 +1,15 @@
-def calculate_distance(x, y):
-    pass
+calculate_distance = lambda x, y: y - x
 
 
-def calculate_segments(a, b):
-    pass
+calculate_segments = lambda a, b: round(a // b)
 
 
-def calculate_digit_sum():
-    pass
+calculate_digit_sum = lambda number: sum(map(int, str(abs(number))))
 
 
-def round_to_multiple():
-    pass
+def round_to_multiple(number, multiple):
+    return multiple * round(number / multiple)
 
 
-def calculate_rect_area():
-    pass
+def calculate_rect_area(x1, y1, x2, y2):
+    return abs(x2 - x1) * abs(y2 - y1)
